2662a3fec586997e39fb833b7063728221939d79
baltamas <70895239+baltamas@users.noreply.github.com>
2021-05-29 14:34:52 +0300

    -project added

---
+++ MultimediaCenter/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MultimediaCenter.Models;
+using MultimediaCenter.ViewModels;
+
+namespace MultimediaCenter
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, MovieViewModel>().ReverseMap();
+            CreateMap<Comment, CommentViewModel>().ReverseMap();
+            CreateMap<Movie, MovieWithCommentsViewModels>().ReverseMap();
+
+        }
+    }
+}
